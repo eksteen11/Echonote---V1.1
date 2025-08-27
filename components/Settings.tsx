9d71b7f8ad9779b3c6ce4472a25a707d64c03c27
eksteen11 <eksteen23@gmail.com>
2025-08-27 20:03:17 +0200

    Fix Settings component: Add type assertion to resolve spread operator TypeScript error

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -93,7 +93,7 @@ export default function Settings() {
     setPreferences(prev => ({
       ...prev,
       [section]: {
-        ...prev[section],
+        ...(prev[section] as any),
         [key]: value,
       },
     }));
